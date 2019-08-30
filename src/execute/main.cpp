da545b0cb4c033d710c8b1cf1ce50c017c61e28f
illidansr <sergei.matyshev@lacit.net>
2019-08-30 17:03:42 +0300

    update - add command line pars

--- src/execute/main.cpp
+++ src/execute/main.cpp
@@ -1,41 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <string>
 #include <boost/filesystem.hpp>
 #include "../face_detect/face_detect.hpp"
 #include "application.hpp"
 
 
-int main() {
-//    std::string path;
-//    std::cout << "IMPUT PATH TO FILE" << std::endl;
-//    std::cin >> path;
-//    face_detect::find_face(path);
-    app::Application application("dss");
-    std::string dirPath = "../resours";
+int main(int argc, char** argv) {
 
-    // Get recursive list of files in given directory and its sub directories
-//    std::vector<std::string> listOfFiles = getAllFilesInDir(dirPath);
-//
-//    // Iterate over the vector and print all files
-//    for (auto str : listOfFiles)
-//        std::cout << str << std::endl;
-//
-//    std::cout << "**********************" << std::endl;
-//
-//    // Get recursive list of files in given directory and skip given folders
+    app::Application application("dss");
+    std::string dirPath;
+    for(int i = 0; i < argc; i++){
+        dirPath = argv[i];
+    }
+
+
     std::vector<std::string> listOfFiles = application.get_all_file_in_dir(dirPath);
 
     for(size_t i = 0; i < listOfFiles.size(); i++){
         boost::filesystem::path filepath = listOfFiles[i];
-        if (filepath.extension() == ".jpg"||filepath.extension() == ".png") // Heed the dot.
+        if (filepath.extension() == ".jpg"||filepath.extension() == ".png")
         {
             face_detect::find_face(listOfFiles[i]);
         }
         else
         {
-            std::cout << listOfFiles[i] << " IS NOT VALID" << std::endl; // Output: e.g. "myFile.cfg is an invalid type"
+            std::cout << listOfFiles[i] << " is not pict" << std::endl;
         }
     }
     return 0;
